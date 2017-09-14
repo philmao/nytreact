823f08f33dd79679af54da38c02c5dfd788a6b87
philmao <philip_mao@yahoo.com>
2017-09-14 14:50:34 -0700

    fix history by using earlier package version

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // Include the Main React Dependencies
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router";
 import Main from "./components/Main";
 
 const App = () =>
